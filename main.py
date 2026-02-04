4a773948bc02a92db2d1bee96bb40e2d411f96bd
malmezat-gif <malmezat.thibault@gmail.com>
2026-02-04 08:57:15 +0100

    Create main.py with game interface implementation

---
+++ main.py
@@ -0,0 +1,14 @@
+# Game Interface Implementation
+
+class Game:
+    def start(self):
+        pass
+
+    def stop(self):
+        pass
+
+    def update(self):
+        pass
+
+    def render(self):
+        pass
