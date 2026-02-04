d482da69164152563ee51e8997917f6ce1f8e361
malmezat-gif <malmezat.thibault@gmail.com>
2026-02-04 09:14:36 +0100

    Add tree manipulation functions in outils.py

--- outils.py
+++ outils.py
@@ -1,1 +1,12 @@
+def get_racine(arbre):
+    if arbre == []:
+        return None
+    return arbre[0]
 
+def get_sous_arbres(arbre):
+    if arbre == []:
+        return []
+    return arbre[1:]
+
+def est_une_feuille(arbre):
+    return len(arbre) == 1
